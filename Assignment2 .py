c6a52dc6403019373e71dbb2621ee35b08263975
Tugrul Kalaycioglu <125144594+KaXaBlanka@users.noreply.github.com>
2023-10-19 23:03:55 +0300

    Update Assignment2 .py

--- Assignment2 .py
+++ Assignment2 .py
@@ -1,1 +1,14 @@
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    num3 = float(input("Enter the third number: "))
+    
+    if num1 == num2 == num3 == 0:
+        break
 
+    total = num1 + num2 + num3
+
+    if num1 == num2 == num3:
+        total *= 3
+
+    print(f"The sum is: {total}")

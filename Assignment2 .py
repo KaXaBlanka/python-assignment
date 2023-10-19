c712cd5f9d7934cb37d73f775cfc937632998537
Tugrul Kalaycioglu <125144594+KaXaBlanka@users.noreply.github.com>
2023-10-19 23:18:22 +0300

    Assignment2 .py

--- Assignment2 .py
+++ Assignment2 .py
@@ -1,3 +1,4 @@
+#Problem=1 
 while True:
     num1 = float(input("Enter the first number: "))
     num2 = float(input("Enter the second number: "))
@@ -12,3 +13,35 @@ while True:
         total *= 3
 
     print(f"The sum is: {total}")
+
+
+#Problem=2
+import random
+
+print("Welcome to the Smart Number Guessing Game!")
+print("I will guess the number you're thinking of in 5 questions or less.")
+
+low = 0
+high = 100
+attempts = 0
+
+while True:
+    guess = random.randint(low, high)
+    print(f"Is your number {guess}?")
+    response = input("Enter 'h' for higher, 'l' for lower, or 'c' for correct: ").lower()
+
+    if response == 'c':
+        print(f"Great! I guessed your number {guess} in {attempts} attempts!")
+        break
+    elif response == 'h':
+        low = guess + 1
+    elif response == 'l':
+        high = guess - 1
+    else:
+        print("Please enter 'h', 'l', or 'c' as your response.")
+
+    attempts += 1
+
+    if attempts >= 5:
+        print("I couldn't guess your number in 5 questions. You win!")
+        break
